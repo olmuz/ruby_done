98529e2df30f43485e4216aed3791c50efbae8cd
oleh <olmuz@outlook.com>
2017-10-01 11:53:56 +0300

    added callback in module and created classmethod within

--- rubyscreencasts/init.rb
+++ rubyscreencasts/init.rb
@@ -31,17 +31,10 @@ p cart.items
 p item1.respond_to?(:weight) # узнать отвечает ли обьект item1 на вызов метода-геттера weight
 p item2.respond_to?(:weight)
 
-=end
-item1 = VirtualItem.new({:name => "Car", :price => 10, :weight => 100})
-item2 = RealItem.new({:name => "Car", :weight => 100, :price => 10,})
-item3 = RealItem.new({:name => "Dishwasher", :weight => 100, :price => 10})
 
 #puts item1.price
 #puts item1.real_price
 
-cart = Cart.new
-cart.add_item item1
-cart.add_item item2
 cart.remove_item
 puts cart.items.size
 
@@ -53,3 +46,15 @@ order.remove_item
 puts order.items.size
 
 puts order.count_valid_items
+=end
+
+item1 = VirtualItem.new({:name => "Car", :price => 101, :weight => 100})
+item2 = RealItem.new({:name => "Car", :weight => 101, :price => 101,})
+item3 = RealItem.new({:name => "Dishwasher", :weight => 10, :price => 10})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+cart.add_item item3
+
+puts cart.items.size
--- rubyscreencasts/item_container.rb
+++ rubyscreencasts/item_container.rb
@@ -1,9 +1,19 @@
 module ItemContainer
 
-  module Manager
-    
+  module ClassMethods
+
+    def min_price # self убираем <= это отсается методом класса
+      100
+    end
+
+  end
+
+  module InstanceMethods
+
     def add_item(item)
-      @items.push(item)
+      unless item.price < self.class.min_price
+        @items.push(item)
+      end  
     end
 
     def remove_item
@@ -13,19 +23,23 @@ module ItemContainer
     def validate
       @items.each { |i| puts "Item has no price" if i.price.nil?}
     end
-    
+      
     def delite_invalid_items
       @items.delete_if {|i| i.price.nil?}
     end
-
-  end
-
-  module Info
 
     def count_valid_items
       @items.count {|i| i.price}
     end
 
+  end
+
+  # метод вызывается каждый раз когда модуль куда-нибудь инклудится
+  def self.included(base)  # в base попадает тот класс в который инклудится модуль  
+    base.extend ClassMethods
+    base.class_eval do
+      include InstanceMethods
+    end
   end
 
 end
